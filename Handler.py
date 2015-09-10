dc10dc1d94ec57c365057e852deebe24f9005003
Jueast Wang <jueastwang@outlook.com>
2015-09-10 17:01:43 +0800

    before dinner

--- Handler.py
+++ Handler.py
@@ -17,8 +17,8 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(template, **params):
+    def render_str(self, template, **params):
         return render_str(template, **params)
 
-    def render(self, template, **params):
-        self.write(self.render_str(template, **params))
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import lib.utils
+import os
+import webapp2
+import jinja2
+import Handler
+
+from google.appengine.ext import db
+
+
+class Post(db.Model):
+
+    username = db.StringProperty(required=True)
+    content = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    up = db.IntegerProperty(required=True)
+    down = db.IntegerProperty(required=True)
+
+    def getScore(self):
+        # calculate the post score from ups, downs and created time
+        # TODO
+        return 0
+
+
+class FrontHandler(Handler.Handler):
+
+    def get(self):
+        self.render("front.html")
+
+
+app = webapp2.WSGIApplication([('/', FrontHandler)
+                               ],
+                              debug=True)
+
